59bd77d5fb29b20c04d45ff2bb644b354f5480ae
masahide-hozo <jonga4311.gdlv@gmail.com>
2020-05-17 17:01:19 +0900

    キャラクター移動ファイルを追加

---
+++ Assets/Project/Program/BattleScene/Scripts/CharacterMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.05F;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 矢印キーの入力情報を取得
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        // 移動する向きを作成する
+        Vector2 direction = new Vector2(h, v).normalized;
+
+        // 移動する向きとスピードを代入 
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+    }
+}
---
+++ Assets/Project/Program/BattleScene/Scripts/GroundCheck.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private string groundTag = "Ground";
+    private bool isGround = false;
+    private bool isGroundEnter, isGroundStay, isGroundExit;
+
+    //接地判定を返すメソッド
+    public bool IsGround()
+    {
+        if (isGroundEnter || isGroundStay)
+        {
+            isGround = true;    
+        }
+        else if (isGroundExit)
+        {
+            isGround = false;
+        }
+
+        isGroundEnter = false;
+        isGroundStay = false;
+        isGroundExit = false;
+        return isGround;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundEnter = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundStay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundExit = true;
+        }
+    }
+}
