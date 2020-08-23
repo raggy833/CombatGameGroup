eb55db40951b7d22f2b1b76a918efd59cfe7912a
Raggy <raggym833@gmail.com>
2020-08-23 16:00:26 +0900

    各画面のベース作成 画面遷移のベース作成

---
+++ Assets/Project/Program/_MainMenu/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // ボタンを押した時の処理
+    public void OnClickChangeSceneButton(string sceneName)
+    {
+        // 引数の画面に遷移する
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
