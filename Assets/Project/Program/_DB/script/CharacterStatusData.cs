b88c163af00ae0b681e88eb6dc706228dad5e137
DaisukeMaruno <daisukemaruno1205@gmail.com>
2020-09-21 16:33:48 +0900

    [GM-2]:view_character_status

---
+++ Assets/Project/Program/_DB/script/CharacterStatusData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//キャラクター用のDatabase
+[CreateAssetMenu(
+  fileName = "CharacterStatusData",
+  menuName = "ScriptableObject/CharacterStatusData",
+  order = 0)
+]
+public class CharacterStatusData : ScriptableObject
+{
+    public List<CharacterStatus> CharacterStatusList = new List<CharacterStatus>(); 
+}
+
+//System.Serializableを設定しないと、データを保持できない(シリアライズできない)ので注意
+[System.Serializable]
+public class CharacterStatus
+{
+
+    //設定したいデータの変数
+    public string Name = "なまえ";
+    public int HP = 100, SP = 50, Atk = 5, Def = 15, Spd = 99;
+    //public bool IsBoss = false;
+
+}
