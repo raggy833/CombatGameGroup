ca9898b00785a8c6fbb5717e8c8f6c580cb1805a
Raggy <raggym833@gmail.com>
2020-10-10 12:43:00 +0900

    CharacterSelectManager.csのアップデート キャラを選択した際にキャラのデータを画面に反映

--- Assets/Project/Program/_DB/script/CharacterStatusData.cs
+++ Assets/Project/Program/_DB/script/CharacterStatusData.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //キャラクター用のDatabase
 [CreateAssetMenu(
-  fileName = "CharacterStatusData",
-  menuName = "ScriptableObject/CharacterStatusData",
-  order = 0)
+    fileName = "CharacterStatusData",
+    menuName = "ScriptableObject/CharacterStatusData",
+    order = 0)
 ]
+[System.Serializable]
 public class CharacterStatusData : ScriptableObject
 {
-    public List<CharacterStatus> CharacterStatusList = new List<CharacterStatus>(); 
+
+    public List<CharacterStatus> characterStatusList = new List<CharacterStatus>();
 }
+
+
 
 //System.Serializableを設定しないと、データを保持できない(シリアライズできない)ので注意
 [System.Serializable]
@@ -22,5 +27,6 @@ public class CharacterStatus
     public string Name = "なまえ";
     public int HP = 100, SP = 50, Atk = 5, Def = 15, Spd = 99;
     //public bool IsBoss = false;
+    public Sprite image;
 
 }
--- Assets/Project/Program/_MainMenu/Scripts/GameManager.cs
+++ Assets/Project/Program/_MainMenu/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -9,14 +10,22 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     //TODO ①デフォルト値をどうするか検討 <--初めてのLogin時
     //TODO ②再起動した時にどこで各ID情報を保持するのか <--2回目以降のLogin時
-    public static int character_id;     //CharacterSelect画面で選択されたキャラクターのID
-    public static int stage_id;         //StageSelect画面で選択されたステージのID
-    public static int gameMode_id;      //GameSelect画面で選択されたゲームのID
-    public static Dictionary<string, int> settings;   //Setting画面で選択した各種設定のIDの配列
+
+    //CharacterSelect画面で選択されたキャラクターのID
+    public static int character_id;
+    //StageSelect画面で選択されたステージのID
+    public static int stage_id;
+    //GameSelect画面で選択されたゲームのID
+    public static int gameMode_id;
+    //Setting画面で選択した各種設定のIDの配列
+
+    public CharacterStatusData characterStatusData;
+    public Image characterImage;
+    public static Dictionary<string, int> settings;
 
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
         }
@@ -34,29 +43,22 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(sceneName);
     }
 
-    //各ID変更(character_id, stage_id, gameMode_id)
-    public void ChangeId (string targetId, int id)
+    // CharacterIdの変更
+    public void ChangeCharacterId(int id)
     {
-        switch (targetId)
-        {
-            case "character":
-                character_id = id;
-                Debug.Log("character_id = " + character_id);
-                break;
-            case "stage":
-                stage_id = id;
-                Debug.Log("stage_id = " + stage_id);
-                break;
-            case "gameMode":
-                gameMode_id = id;
-                Debug.Log("gameMode_id = " + gameMode_id);
-                break;
-            default:
-                Debug.Log("ID is not change!!");
-                break;
-        }
-
+        character_id = id;
     }
+    // StageIDの変更
+    public void ChangeStageId(int id)
+    {
+        stage_id = id;
+    }
+    // GameIdの変更
+    public void ChangeGameId(int id)
+    {
+        gameMode_id = id;
+    }
+
 
     //TODO Settingの変更methodの検討
     //Setting内容変更
